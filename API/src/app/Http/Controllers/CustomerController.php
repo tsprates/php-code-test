4a362cfbcaf10cb452cd975b7c8e3cd3fd218ebf
Thiago Prates <tsprates@gmail.com>
2022-02-21 14:12:34 -0300

    init API

---
+++ API/src/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *P
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validation = $this->makeValidation($data);
+
+        if (!$validation->fails()) {
+            return response()->json([
+                'success' => true
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+            'errors' => $validation->errors(),
+        ], 422);
+    }
+
+    /**
+     * Makes a customer validator.
+     * 
+     * @param array $customerArray 
+     * @return ValidationValidator 
+     */
+    private function makeValidation(array $customerArray)
+    {
+        return Validator::make($customerArray,  [
+            'name' => 'required|min:5',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'address.number' => 'required|integer|min:1',
+            'address.street' => 'required|min:5',
+            'address.city' => 'required|min:5',
+            'address.state' => 'required|min:5',
+            'address.country' => 'required|min:5',
+        ]);
+    }
+}
---
+++ API/src/tests/Feature/CustomerTest.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Tests\Feature;
+
+use Faker\Factory as Faker;
+use Tests\TestCase;
+
+class CustomerTest extends TestCase
+{
+    private static $faker;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        static::$faker = Faker::create();
+    }
+
+    /**
+     * Helper function for mocking customers.
+     * 
+     * @return array Customer array.
+     */
+    private function mockCustomer()
+    {
+        $faker = static::$faker;
+
+        return [
+            'name' =>  $faker->name(),
+            'email' => $faker->safeEmail(),
+            'phone' => $faker->phoneNumber(),
+            'address' => [
+                'number' => $faker->buildingNumber(),
+                'street' => $faker->streetName(),
+                'city' => $faker->city(),
+                'state' => $faker->state(),
+                'country' => $faker->country(),
+            ]
+        ];
+    }
+
+    public function test_create_new_customer()
+    {
+        $response = $this->post('/api/customers', $this->mockCustomer());
+        $response->assertStatus(201);
+        $response->assertJsonFragment(['success' => true]);
+    }
+
+    public function test_tries_to_create_a_customer_without_name()
+    {
+        $customer =  $this->mockCustomer();
+        unset($customer['name']);
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_with_name_length_less_than_five_letters()
+    {
+        $customer =  $this->mockCustomer();
+        $customer['name'] = ' '; // invalid name
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_with_invalid_email()
+    {
+        $customer = $this->mockCustomer();
+        $customer['email'] = 'invalid';
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_without_email()
+    {
+        $customer = $this->mockCustomer();
+        unset($customer['email']);
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_without_phone_number()
+    {
+        $customer = $this->mockCustomer();
+        unset($customer['phone']);
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_with_invalid_house_number()
+    {
+        $customer = $this->mockCustomer();
+        $customer['address']['number'] = 'invalid';
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment([
+            'success' => false,
+            "address.number" => [
+                "The address.number must be an integer."
+            ]
+        ]);
+    }
+
+    public function test_tries_to_create_a_customer_with_wrong_house_number()
+    {
+        $customer = $this->mockCustomer();
+        $customer['address']['number'] = '0'; // invalid zero number
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment([
+            'success' => false,
+            "address.number" => [
+                "The address.number must be at least 1."
+            ]
+        ]);
+    }
+
+    public function test_tries_to_create_a_customer_without_house_number()
+    {
+        $customer = $this->mockCustomer();
+        unset($customer['address']['number']);
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_without_street()
+    {
+        $customer = $this->mockCustomer();
+        unset($customer['address']['street']);
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_without_city()
+    {
+        $customer = $this->mockCustomer();
+        unset($customer['address']['city']);
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+
+    public function test_tries_to_create_a_customer_without_country()
+    {
+        $customer = $this->mockCustomer();
+        unset($customer['address']['country']);
+        
+        $response = $this->post('/api/customers', $customer);
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['success' => false]);
+    }
+}
